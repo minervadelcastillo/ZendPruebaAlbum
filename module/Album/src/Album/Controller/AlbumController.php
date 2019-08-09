c607bbd6b3e1838f4810312b60e858ae4b8b9a28
vagrant <vagrant@homestead>
2019-08-09 12:01:10 +0000

    Borrado comentarios

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -108,7 +108,7 @@ class AlbumController extends AbstractActionController
             )
         );
 
-        
+
 
         $dataArray = array();
         $albums = $this->getAlbumTable()->fetchAll();
@@ -122,29 +122,20 @@ class AlbumController extends AbstractActionController
         $data = array_merge($titulos, $dataArray);
 
 
-//        var_dump($dataArray);
-        // create php excel object
+
         $doc = new \PHPExcel();
 
-// set active sheet
         $doc->setActiveSheetIndex(0);
-        /*->SetHeaderCells('B1', 'Titulo')
-        ->SetHeaderCells('C1', 'Artista');*/
-// read data to active sheet
+
         $doc->getActiveSheet()->fromArray($data);
 
-//save our workbook as this file name
         $filename = 'album.xlsx';
-//mime type
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="' . $filename);
         header('Cache-Control: max-age=0'); //no cache
-//save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
-//if you want to save it as .XLSX Excel 2007 format
 
         $objWriter = \PHPExcel_IOFactory::createWriter($doc, 'Excel2007');
 
-//force user to download the Excel file without writing it to server's HD
         $objWriter->save('php://output');
 
     }
